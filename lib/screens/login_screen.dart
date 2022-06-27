783d41062b904700a7abede242a9e1fad820f823
vachtung-gigabidze <dmitry.zyadik@gmail.com>
2022-06-27 22:06:13 +0500

    add textfields to login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:otus_food_app/constants.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -13,7 +13,7 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
+      backgroundColor: const Color(0xFF2ECC71),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -28,16 +28,187 @@ class _LoginScreenState extends State<LoginScreen> {
                 color: Color(0xFFFFFFFF),
               ),
             ),
-            //  TextField(),
-            //   TextField(),
-            //   TextField(),
+            const SizedBox(
+              height: 81,
+            ),
+            SizedBox(
+              height: 48,
+              width: 232,
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  hoverColor: Colors.white,
+                  prefixIcon: GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        padding: const EdgeInsets.all(10.0),
+                        color: Colors.transparent,
+                        constraints: const BoxConstraints(
+                          maxHeight: 24.0,
+                          maxWidth: 24.0,
+                        ),
+                        child: const Image(
+                          image: AssetImage(
+                            Constants.iconProfile,
+                          ),
+                          color: Color(0xFFC2C2C2),
+                          height: 24,
+                          width: 24,
+                        ),
+                      )),
+                  enabledBorder: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    borderSide: BorderSide(
+                      color: Color(0xFFFFFFFF),
+                      width: 2,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  focusedBorder: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    borderSide: BorderSide(
+                      color: Color(0xFFFFFFFF),
+                      width: 2,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  hintText: 'логин',
+                  hintStyle: const TextStyle(
+                    height: 2.0,
+                    fontFamily: 'Roboto',
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 16.0,
+                    color: Color(0xFFC2C2C2),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            SizedBox(
+              height: 48,
+              width: 232,
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  hoverColor: Colors.white,
+                  prefixIcon: GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        padding: const EdgeInsets.all(10.0),
+                        color: Colors.transparent,
+                        constraints: const BoxConstraints(
+                          maxHeight: 24.0,
+                          maxWidth: 24.0,
+                        ),
+                        child: const Image(
+                          image: AssetImage(
+                            Constants.iconProfile,
+                          ),
+                          color: Color(0xFFC2C2C2),
+                          height: 24,
+                          width: 24,
+                        ),
+                      )),
+                  enabledBorder: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    borderSide: BorderSide(
+                      color: Color(0xFFFFFFFF),
+                      width: 2,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  focusedBorder: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    borderSide: BorderSide(
+                      color: Color(0xFFFFFFFF),
+                      width: 2,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  hintText: 'пароль',
+                  hintStyle: const TextStyle(
+                    height: 2.0,
+                    fontFamily: 'Roboto',
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 16.0,
+                    color: Color(0xFFC2C2C2),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            SizedBox(
+              height: 48,
+              width: 232,
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  hoverColor: Colors.white,
+                  prefixIcon: GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        padding: const EdgeInsets.all(10.0),
+                        color: Colors.transparent,
+                        constraints: const BoxConstraints(
+                          maxHeight: 24.0,
+                          maxWidth: 24.0,
+                        ),
+                        child: const Image(
+                          image: AssetImage(
+                            Constants.iconProfile,
+                          ),
+                          color: Color(0xFFC2C2C2),
+                          height: 24,
+                          width: 24,
+                        ),
+                      )),
+                  enabledBorder: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    borderSide: BorderSide(
+                      color: Color(0xFFFFFFFF),
+                      width: 2,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  focusedBorder: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    borderSide: BorderSide(
+                      color: Color(0xFFFFFFFF),
+                      width: 2,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  hintText: 'пароль еще раз',
+                  hintStyle: const TextStyle(
+                    height: 2.0,
+                    fontFamily: 'Roboto',
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 16.0,
+                    color: Color(0xFFC2C2C2),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
             TextButton(
               onPressed: () {},
               child: Text('Войти'),
             ),
             TextButton(
               onPressed: () {},
-              child: Text('Зарегистрироваться'),
+              child: const Text('Зарегистрироваться'),
             ),
           ],
         ),
