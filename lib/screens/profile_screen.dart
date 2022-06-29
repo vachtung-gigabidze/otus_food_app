05b6a0ef561cb371ea8fb9db96bacb5fd7cddaa1
vachtung-gigabidze <63574696+vachtung-gigabidze@users.noreply.github.com>
2022-06-29 09:02:50 +0500

    complite profile view

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -58,14 +58,15 @@ class ProfileScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
-              height: 250,
-              width: 250,
+              height: 123,
+              width: 123,
+              padding: EdgeInsets.all(13),
               decoration: BoxDecoration(
                 color: Colors.white,
                 shape: BoxShape.circle,
                 border: Border.all(
-                  color: Colors.green,
-                  width: 5,
+                  color: Color(0xFF165932),
+                  width: 4,
                 ),
               ),
               child: Image.asset(
@@ -73,6 +74,68 @@ class ProfileScreen extends StatelessWidget {
                 height: 96,
                 width: 96,
                 fit: BoxFit.fitWidth,
+              ),
+            ),
+            const SizedBox(
+              height: 29,
+            ),
+            Container(
+              width: 396.0,
+              height: 64.0,
+              decoration: const BoxDecoration(
+                color: Color(0xFFFFFFFF),
+                borderRadius: BorderRadius.all(Radius.circular(5.0)),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text(
+                      'Логин',
+                      style: TextStyle(
+                        fontFamily: 'Roboto',
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 16.0,
+                        color: Color(0xFF165932),
+                      ),
+                    ),
+                    Text(
+                      'avpetrov',
+                      style: TextStyle(
+                        fontFamily: 'Roboto',
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 16.0,
+                        color: Color(0xFF165932),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Container(
+              width: 396.0,
+              height: 64.0,
+              decoration: const BoxDecoration(
+                color: Color(0xFFFFFFFF),
+                borderRadius: BorderRadius.all(Radius.circular(5.0)),
+              ),
+              child: const Center(
+                child: Text(
+                  'Выход',
+                  style: TextStyle(
+                    fontFamily: 'Roboto',
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 16.0,
+                    color: Color(0xFFF54848),
+                  ),
+                ),
               ),
             ),
           ],
