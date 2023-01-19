d45bd199e00b2f14d0391f0ec65c2cad4eb21776
vachtung-gigabidze <dmitry.zyadik@gmail.com>
2023-01-19 16:57:11 +0500

    add widget test heart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,8 +1,57 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:otus_food_app/feature/recipe_list/ui/components/detail/heart.dart';
+import 'package:rive/rive.dart';
+
+Widget _wrap(Widget widget) {
+  return MaterialApp(
+    home: Scaffold(body: widget),
+  );
+}
 
 void main() {
-  testWidgets("Виджет-тест для переключения избранного",
-      (WidgetTester tester) async {
-    /// Тело теста
+  testWidgets("Виджет-тест для переключения избранного", (widgetTester) async {
+    bool isFavorite = false;
+    int counter = 0;
+
+    cb(String id) {
+      isFavorite = !isFavorite;
+      counter++;
+      log('tap');
+    }
+
+    await widgetTester.pumpWidget(_wrap(HeartWidget(
+      animate: isFavorite,
+      heatTap: () => cb('1'),
+    )));
+
+    final image = find.byType(Image);
+
+    expect(image, findsOneWidget);
+    expect(
+      find.byWidgetPredicate((widget) => widget is Image),
+      findsOneWidget,
+    );
+
+    expect(isFavorite, false);
+
+    await widgetTester.tap(find.byType(InkWell), pointer: 1);
+    await widgetTester.pump(const Duration(seconds: 1));
+    await widgetTester.pumpAndSettle();
+
+    expect(counter, 1);
+
+    final river = find.byType(RiveAnimation);
+
+    expect(river, findsOneWidget);
+    expect(
+      find.byWidgetPredicate((widget) =>
+          widget is RiveAnimation && 'assets/animate/heart.riv' == widget.name),
+      findsOneWidget,
+    );
+
+    expect(isFavorite, true);
   });
 }
